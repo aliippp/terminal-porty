a01515829bfb96cad79abfd51921109baf4ab870
alphaa <alifadha1@gmail.com>
2024-04-25 13:33:42 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+function App() {
+	return (
+		<>
+			<div className=" h-screen bg-black text-white">
+				<div className="float-right mr-2">aliefadha</div>
+				<div className="">
+					<table className="w-full table-auto text-sm">
+						<tbody>
+							<tr className="border-b-gray-600 border-b">
+								<div className="py-5 text-lime-300">
+									<span className="block text-gray-600">~ (0.02s)</span>
+									Currently building...
+								</div>
+							</tr>
+							<tr className="border-b-gray-600 border-b">
+								<div className="py-5 text-lime-300">
+									<span className="block text-gray-600">~ (0.01s)</span>It
+									supposed to be my pages but idk what would the content be
+									lmao.
+								</div>
+							</tr>
+							<tr className="border-b-gray-600 border-b">
+								<div className="py-5 text-lime-300">
+									<span className="block text-gray-600">~ (0.01s)</span>Thanks
+									for clicking btw.
+								</div>
+							</tr>
+							<tr className="">
+								<div className="py-5 text-white">
+									<span className="block text-pink-400">~</span>
+									<input className="bg-black outline-none"></input>
+								</div>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
